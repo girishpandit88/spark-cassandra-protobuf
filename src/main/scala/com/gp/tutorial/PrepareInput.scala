f4a05eec4e0b003f6e78d6ae5fdf1c8f81550850
Girish Pandit <girschol7@gmail.com>
2020-07-26 13:56:57 -0700

    scalafmt

--- src/main/scala/com/gp/tutorial/PrepareInput.scala
+++ src/main/scala/com/gp/tutorial/PrepareInput.scala
@@ -6,38 +6,39 @@ import javax.xml.bind.DatatypeConverter
 import scala.collection.mutable
 
 object PrepareInput {
-	val address = Address("3rd Street", "CA", 98029)
-	val properties = Property(Map("key" -> Value()
-		.withStringValue("foo")
-		.withDoubleValue(2)
-		.withLongValue(4l)))
-	var students =
-		new mutable.ListBuffer[Student]()
+  val address = Address("3rd Street", "CA", 98029)
+  val properties = Property(
+    Map(
+      "key" -> Value()
+        .withStringValue("foo")
+        .withDoubleValue(2)
+        .withLongValue(4l)
+    )
+  )
+  var students =
+    new mutable.ListBuffer[Student]()
 
-	(1 to 100)
-		.foreach(i =>
-			students :+= Student()
-				.update(
-					_
-						.name := s"Joe_$i",
-					_
-						.age := 32,
-					_
-						.address := address,
-					_
-						.properties := properties,
-					_
-						.id := i)
-		)
+  (1 to 100)
+    .foreach(
+      i =>
+        students :+= Student()
+          .update(
+            _.name := s"Joe_$i",
+            _.age := 32,
+            _.address := address,
+            _.properties := properties,
+            _.id := i
+        )
+    )
 
-	def main(args: Array[String]) {
-		students
-			.foreach {
-				p =>
-					println(DatatypeConverter
-						.printBase64Binary(p
-							.toByteArray))
-			}
-	}
+  def main(args: Array[String]) {
+    students
+      .foreach { p =>
+        println(
+          DatatypeConverter
+            .printBase64Binary(p.toByteArray)
+        )
+      }
+  }
 
 }
